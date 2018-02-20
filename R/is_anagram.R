c7b0dac2fae1a1c1001072b47ea3fea9c8e7ad14
ChrisMuir <chrismuirRVA@gmail.com>
2018-02-20 02:20:29 -0600

    add arg 'ignore_spaces'

--- R/is_anagram.R
+++ R/is_anagram.R
@@ -7,7 +7,9 @@
 #' @param terms Character vector.
 #' @param any_len Logical, should the function be looking for anagrams that
 #'  are any length, or be restricted to anagrams that are the same length as
-#'  input arg \code{string}.
+#'  input arg \code{string}. Default value is FALSE.
+#' @param ignore_spaces, logical, should spaces within input arg \code{string}
+#'  be ignored when searching for anagrams. Default value is FALSE.
 #'
 #' @return Logical vector, with length equal to that of \code{terms}.
 #' @export
@@ -16,13 +18,21 @@
 #' str_to_check <- c("cats are great", "tacs", "frogs", "cats", "ts")
 #' is_anagram("cats", str_to_check)
 #' is_anagram("cats", str_to_check, any_len = TRUE)
+#' is_anagram("c a t s", str_to_check, ignore_spaces = TRUE)
 #'
 #' @useDynLib anagrams
 #' @importFrom Rcpp sourceCpp
-is_anagram <- function(string, terms, any_len = FALSE) {
+is_anagram <- function(string, terms, any_len = FALSE, ignore_spaces = FALSE) {
   stopifnot(is.character(string) && length(string) == 1)
   stopifnot(is.character(terms))
   stopifnot(is.logical(any_len))
+
+  # If ignore_spaces is TRUE, remove all spaces in both string and terms.
+  if (ignore_spaces) {
+    string <- gsub(" ", "", string, fixed = TRUE)
+    terms <- gsub(" ", "", terms, fixed = TRUE)
+  }
+
   .Call('_anagrams_cpp_is_anagram', PACKAGE = 'anagrams', string, terms,
         any_len)
 }
