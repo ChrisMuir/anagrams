fb7995715a88cd1e56abfdf6d277f3833cbb28ca
ChrisMuir <chrismuirRVA@gmail.com>
2018-02-20 02:26:16 -0600

    add arg 'ignore_case'

--- R/is_anagram.R
+++ R/is_anagram.R
@@ -8,8 +8,10 @@
 #' @param any_len Logical, should the function be looking for anagrams that
 #'  are any length, or be restricted to anagrams that are the same length as
 #'  input arg \code{string}. Default value is FALSE.
-#' @param ignore_spaces, logical, should spaces within input arg \code{string}
-#'  be ignored when searching for anagrams. Default value is FALSE.
+#' @param ignore_spaces logical, should spaces be ignored when searching for
+#'  anagrams. Default value is FALSE.
+#' @param ignore_case logical, should case (lower/upper) be ignored when
+#'  searching for anagrams. Default value is FALSE.
 #'
 #' @return Logical vector, with length equal to that of \code{terms}.
 #' @export
@@ -19,18 +21,28 @@
 #' is_anagram("cats", str_to_check)
 #' is_anagram("cats", str_to_check, any_len = TRUE)
 #' is_anagram("c a t s", str_to_check, ignore_spaces = TRUE)
+#' is_anagram("CATs", str_to_check, ignore_case = TRUE)
 #'
 #' @useDynLib anagrams
 #' @importFrom Rcpp sourceCpp
-is_anagram <- function(string, terms, any_len = FALSE, ignore_spaces = FALSE) {
+is_anagram <- function(string, terms, any_len = FALSE, ignore_spaces = FALSE,
+                       ignore_case = FALSE) {
   stopifnot(is.character(string) && length(string) == 1)
   stopifnot(is.character(terms))
   stopifnot(is.logical(any_len))
+  stopifnot(is.logical(ignore_spaces))
+  stopifnot(is.logical(ignore_case))
 
   # If ignore_spaces is TRUE, remove all spaces in both string and terms.
   if (ignore_spaces) {
     string <- gsub(" ", "", string, fixed = TRUE)
     terms <- gsub(" ", "", terms, fixed = TRUE)
+  }
+
+  # If ignore_case is TRUE, make all input strings lower-case.
+  if (ignore_case) {
+    string <- tolower(string)
+    terms <- tolower(terms)
   }
 
   .Call('_anagrams_cpp_is_anagram', PACKAGE = 'anagrams', string, terms,
